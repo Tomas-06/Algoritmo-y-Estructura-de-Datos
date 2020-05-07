371051ed18e15f1e35c5d7ed8142bd57cc735c56
Tomas-06 <63795151+Tomas-06@users.noreply.github.com>
2020-05-07 16:37:01 -0300

    Add files via upload

---
+++ 01-CppAdicion/Adicion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main () {
+using namespace std;
+int A, B;
+cout <<"Su primer numero es\n";
+cin >> A;
+cout <<"Su segundo numero es \n";
+cin >> B;
+cout <<"Su respuesta es\n";
+cout << A + B;
+
+
+}
