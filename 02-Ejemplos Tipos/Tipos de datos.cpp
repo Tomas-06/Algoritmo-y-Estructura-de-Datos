be071037a862f8ba17e54b6f0c3624804974a6bb
Tomas-06 <63795151+Tomas-06@users.noreply.github.com>
2020-05-21 10:23:40 -0300

    Add files via upload

---
+++ 02-Ejemplos Tipos/Tipos de datos.cpp
@@ -0,0 +1,47 @@
+/*Ejemplos de valores y operaciones de tipos de datos
+Tomas Quintana
+2020-14-05
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int entero = 6;
+    float flotante = 1.69;
+    char letra[30] = "T";
+    unsigned numero = (-10)    ;
+
+    cout<<entero<<endl;
+    cout<<flotante<<endl;
+    cout<<letra<<endl;
+    cout<<numero<<endl;
+
+    int T = 3 > 5;
+    if (T){
+        cout<<"verdadero"<<endl;
+         } else {
+             cout<<"falso"<<endl;
+         } 
+
+        
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+    system("PAUSE(0)");
+}
+
